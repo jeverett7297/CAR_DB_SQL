6a6e33af947407383295947ed88526d58fcec5ef
Joseph Everett <joseph.everett@hotmail.com>
2017-06-09 09:03:31 -0700

    Add files via upload

---
+++ SiteCoreData_IndexBuild.sql
@@ -0,0 +1,19 @@
+alter table dbo.sitecoredata alter column objectid int not null;
+alter table dbo.sitecoredata alter column t_objectid int not null;
+
+alter table dbo.sitecoredata alter column updatedate nvarchar(max) not null;
+alter table dbo.sitecoredata alter column updatedate datetime not null;
+alter table dbo.sitecoredata add ID int IDENTITY(1,1) not null
+
+alter table dbo.sitecoredata add primary key (objectid, ID)
+
+select objectid, count(*) from dbo.sitecoredata
+group by objectid having count(objectid) > 1
+
+select * from sitecoredata where objectid in(
+select objectid from dbo.sitecoredata
+group by objectid having count(objectid) > 1)
+order by objectid, t_objectid
+
+create nonclustered index IX_NC_ObjectID
+on dbo.sitecoredata (objectid)
