332184b21ec0de83b83ebc788a23042e6d8171a1
Joseph Everett <joseph.everett@hotmail.com>
2017-06-13 13:22:51 -0700

    Add files via upload

--- SiteCoreData_IndexBuild.sql
+++ SiteCoreData_IndexBuild.sql
@@ -7,13 +7,19 @@ alter table dbo.sitecoredata add ID int IDENTITY(1,1) not null
 
 alter table dbo.sitecoredata add primary key (objectid, ID)
 
-select objectid, count(*) from dbo.sitecoredata
-group by objectid having count(objectid) > 1
+--select objectid, count(*) from dbo.sitecoredata
+--group by objectid having count(objectid) > 1
 
-select * from sitecoredata where objectid in(
-select objectid from dbo.sitecoredata
-group by objectid having count(objectid) > 1)
-order by objectid, t_objectid
+--select * from sitecoredata where objectid in(
+--select objectid from dbo.sitecoredata
+--group by objectid having count(objectid) > 1)
+--order by objectid, t_objectid
 
 create nonclustered index IX_NC_ObjectID
 on dbo.sitecoredata (objectid)
+
+create nonclustered index IX_NC_ObjectTypeID
+on dbo.sitecoredata (ObjectTypeID)
+
+create nonclustered index IX_NC_ParentObjectID
+on dbo.sitecoredata (ParentObjectID)
